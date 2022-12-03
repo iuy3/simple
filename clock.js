7e9081618962073c3e280844074f69220ad6cefd
hizlle <hizlle@mail.ru>
2022-12-03 16:57:59 +0600

    clock done

--- clock.js
+++ clock.js
@@ -1,17 +1,30 @@
-function Clock(options){
+function Clock(options) {
+    var elem = options.elem
+    var timer;
 
-let elem = options.elem
+    function render() {
+        var date = new Date()
 
-function render(){}
+        var hour = date.getHours()
+        var mins = date.getMinutes()
+        var second = date.getSeconds()
+
+        if (hour < 10) { hour = '0' + hour }
+        elem.querySelector('.hour').innerHTML = hour;
+
+        elem.querySelector('.mins').innerHTML = mins;
+        if (mins < 10) { mins = '0' + mins }
+        elem.querySelector('.sec').innerHTML = second;
+        if (second < 10) { second = '0' + second }
+    }
+
+    this.start = function () {
+        render()
+        timer = setInterval(render, 1000)
+    }
 
 
-
-this.start = function(){}
-
-
-this.stop = function(){}
-
-
-
-
+    this.stop = function () {
+        clearInterval(timer)
+    }
 }
